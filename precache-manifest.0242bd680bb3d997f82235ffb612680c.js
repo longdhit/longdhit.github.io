f443b1a0aa09d1c957239f4551d301e5fac1d5f5
longdhit <longdhit@gmail.com>
2019-04-21 13:30:43 +0700

    Initial commit

---
+++ precache-manifest.0242bd680bb3d997f82235ffb612680c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d388e68f5f8ba1d029e",
+    "url": "/static/js/main.1c794271.chunk.js"
+  },
+  {
+    "revision": "658895d25cc9936e1d89",
+    "url": "/static/js/2.0c799d1c.chunk.js"
+  },
+  {
+    "revision": "0d388e68f5f8ba1d029e",
+    "url": "/static/css/main.b1394318.chunk.css"
+  },
+  {
+    "revision": "658895d25cc9936e1d89",
+    "url": "/static/css/2.9f7c4af5.chunk.css"
+  },
+  {
+    "revision": "a969f3443777fd21fcbcd61a9e40eead",
+    "url": "/index.html"
+  }
+];
